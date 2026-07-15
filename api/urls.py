6048daf6ccff0d754bf8bd175ff1ad4c7561db52
luisar2307 <luisandresarias2007@gmail.com>
2026-07-15 15:29:40 -0500

    soluciones esrrores con make migrations en api/urls

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from accounts.views import UsuarioViewSet, login
 # Import viewsets from apps
 from staff.views import PersonalMedicoViewSet
 from patients.views import PacienteViewSet
 from triage.views import TriajeViewSet
-<<<<<<< HEAD
 from accounts.views import UsuarioViewSet
-=======
-from accounts.views import login
->>>>>>> 48e53ad4fedd972c45de01646c3dcd5c2243b8da
 
 router = DefaultRouter()
 router.register(r"medicos", PersonalMedicoViewSet, basename="personal-medico")
